1c1795a2528e226d4cc39b6eddcc55a9e58585bd
dudawizz <langereduarda7@gmail.com>
2024-05-14 17:26:29 -0300

    primeira experiencia com php

---
+++ 01-hello-world.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando PHP</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+
+    <!-- Delimitadores de abertura e fechamento PHP -->
+    <?php
+    // Comando de saída: echo
+    echo "Olá!";
+    echo "<p style='text-align:center'>Senac Penha</p>";
+    echo "<p style="text-align:center">Senac Penha</p>";
+    echo "<p style=\"text-align:center"\>Senac Penha</p>";
+    ?>
+    <!-- Delimitadores de abertura e fechamento PHP -->
+
+</body>
+</html>
