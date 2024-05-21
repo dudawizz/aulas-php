e21c4ddc29bbb98f96d69353830554680789644d
dudawizz <langereduarda7@gmail.com>
2024-05-21 14:52:25 -0300

    include e require

--- 01-hello-world.php
+++ 01-hello-world.php
@@ -1,23 +1,37 @@
+<?php include "recursos.php"; ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciando PHP</title>
+    <title> <?=EMPRESA?> Iniciando com PHP</title>
+    <style>
+        p { color: red; }
+    </style>
 </head>
 <body>
     <h1>Experiências com PHP</h1>
     <hr>
 
-    <!-- Delimitadores de abertura e fechamento PHP -->
+    <!-- Delimitadores de abertura/fechamento PHP -->
     <?php
     // Comando de saída: echo
     echo "Olá!";
     echo "<p style='text-align:center'>Senac Penha</p>";
-    echo "<p style="text-align:center">Senac Penha</p>";
+    echo '<p style="text-align:center">Senac Penha</p>';
     echo "<p style=\"text-align:center\">Senac Penha</p>";
     ?>
-    <!-- Delimitadores de abertura e fechamento PHP -->
+    <!-- Delimitadores de abertura/fechamento PHP -->
+
+    <hr>
+    <h2>Tecnologias Trabalhadas</h2>
+
+<?php foreach ( $tecnologias as $tecnologia ) { ?>
+    <p> <?=$tecnologia?> </p>
+<?php }
+
+?>
 
 </body>
 </html>
---
+++ recursos.php
@@ -0,0 +1,8 @@
+<?php
+// recursos.php
+const EMPRESA = "ABC Tecnologia";
+$anoFiscal = 2024;
+$tecnologias = ["Python", "IA Generativa", "Machine Learning"];
+
+
+/* Em arquivos que possuem somente código PHP, não é necessário fazer o fechamento "?>" */
