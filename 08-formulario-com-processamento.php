81448298767cec4a2a3fd33130fcf7e8b680a78c
dudawizz <langereduarda7@gmail.com>
2024-05-22 15:49:42 -0300

    removendo formulario apos inscrição

---
+++ 08-formulario-com-processamento.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário com PHP</title>
+</head>
+
+<body>
+    <h1>Formulário e processamento juntos</h1>
+    <hr>
+  
+<?php
+/* Detectando o envio/acionamento do formulário */
+if( isset($_POST['enviar']) ){
+    $nome = $_POST["nome"];
+?>
+    <h2>Bem-vindo(a) <?=$nome?>!</h2>
+    <hr>
+<?php
+} else {
+/* O formulário abaixo será exibido enquanto o botão enviar não for acionado. Quando for, o formulário será removido da página. */
+?>
+
+    <form action="" method="post"> 
+        <label for="nome">Nome:</label> 
+        <input required type="text" name="nome" id="nome"> 
+        <button type="submit" name="enviar">Enviar</button> 
+    </form>
+
+<?php } ?>
+
+</body>
+
+</html>
